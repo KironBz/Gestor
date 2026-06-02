6ce51852178104cc29c5bff488c8c127b1010de5
Hunab <bohorquez.atha@gmail.com>
2026-06-01 19:15:58 -0600

    Yess v 1.6.0

--- App.xaml.cs
+++ App.xaml.cs
@@ -75,14 +75,14 @@ namespace Yes_Gestor
             */
 
             await Servicio.GuardarAsync(Datos);
-            
+
             /*
             var balance = new VentanaBalance();
             balance.Show();
             */
 
-            var movimientos = new VentanaMovimientos();
-            movimientos.Show();
+            var balance = new VentanaBalance();
+            balance.Show();
         }
     }
 }

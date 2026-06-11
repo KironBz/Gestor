18118dfeb9ab6eb609887b81f4bf48cefef6b1eb
Hunab B <88869031+KironBz@users.noreply.github.com>
2026-06-11 16:52:35 +0000

    Solucion Fallida

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using YESS;                           // ← cambiado
-using YESS.Services;                  // ← cambiado
+using YESS;
+using YESS.Services;
 using Radzen;
 using System.Globalization;
 
-// Configurar cultura
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("es-MX");
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("es-MX");
 
@@ -13,14 +12,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// HttpClient base
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-// Registrar servicios (si tienes interfaz, regístrala)
-// builder.Services.AddSingleton<IArchivoService, ArchivoService>();
-builder.Services.AddSingleton<ArchivoService>(); // ← mientras no tengas la interfaz
-
-// Radzen
+builder.Services.AddSingleton<ArchivoService>();
 builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
